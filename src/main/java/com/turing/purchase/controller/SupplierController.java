f3933e40fd9fa7df4f9bad5f861debdbcc676424
luoyuxuwu <593349045@qq.com>
2021-04-19 18:52:07 +0800

    第九次提交，供应商产品分页，查询

--- src/main/java/com/turing/purchase/controller/SupplierController.java
+++ src/main/java/com/turing/purchase/controller/SupplierController.java
@@ -1,5 +1,9 @@
 package com.turing.purchase.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.turing.purchase.entity.EasyUIDataGridJsonEntity;
+import com.turing.purchase.entity.QuoteDetail;
 import com.turing.purchase.entity.Supplier;
 import com.turing.purchase.service.SupplierService;
 import org.apache.shiro.SecurityUtils;
@@ -7,14 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/supplyman")
-//@RestController
 public class SupplierController {
 
     @Autowired
@@ -32,6 +35,29 @@ public class SupplierController {
     public String getLoginId(){
         String loginId = (String)SecurityUtils.getSubject().getPrincipal();
         return loginId;
+    }
+
+    /*@RequestMapping("/getSupplierProductsPage")
+    public String getSupplierProductsPage(
+            @RequestParam(value = "pageNum",required = true,defaultValue = "1")Integer pageNum,
+            @RequestParam(value = "pageSize",required = true,defaultValue = "5")Integer pageSize,
+            HttpSession session){
+        PageHelper.startPage(pageNum,pageSize);
+        List<QuoteDetail> products = supplierService.getSupplierProducts((String) SecurityUtils.getSubject().getPrincipal());
+        PageInfo<QuoteDetail> pageInfo = new PageInfo<>(products);
+        session.setAttribute("pageInfo",pageInfo);
+        return "redirect:/goodsSelect";
+    }*/
+    @RequestMapping("/getSupplierProductsPage")
+    @ResponseBody
+    public EasyUIDataGridJsonEntity getSupplierProductsPage(
+            @RequestParam(value = "page",required = true,defaultValue = "1")Integer pageNum,
+            @RequestParam(value = "rows",required = true,defaultValue = "10")Integer pageSize,
+            @RequestParam(value = "sort",defaultValue = "") String sort,
+            @RequestParam(value = "order",defaultValue = "") String order){
+        EasyUIDataGridJsonEntity daraGrid = supplierService.getSupplierProductsDaraGrid((String) SecurityUtils.getSubject().getPrincipal(), pageNum, pageSize, sort, order);
+
+        return daraGrid;
     }
 
 }
--- src/main/java/com/turing/purchase/service/SupplierService.java
+++ src/main/java/com/turing/purchase/service/SupplierService.java
@@ -1,6 +1,10 @@
 package com.turing.purchase.service;
 
+import com.turing.purchase.entity.EasyUIDataGridJsonEntity;
+import com.turing.purchase.entity.QuoteDetail;
 import com.turing.purchase.entity.Supplier;
+
+import java.util.List;
 
 public interface SupplierService {
 
@@ -11,4 +15,14 @@ public interface SupplierService {
      * @return 供应商对象
      */
     Supplier getSupplierInfo(String userName);
+
+    //查询供应商产品
+    List<QuoteDetail> getSupplierProducts(String userName,Integer pageNum,Integer pageSize,String sort,String order);
+
+    //查询供应商产品（datagrid）
+    EasyUIDataGridJsonEntity getSupplierProductsDaraGrid(String userName,Integer pageNum,Integer pageSize,String sort,String order);
+
+    //获取总条数
+    int getTotalProducts(String userName);
+
 }
